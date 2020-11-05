845601ca7058cb054503da191d6320215e5bf9ff
Simi Francis <simi7francis@gmail.com>
2020-11-05 13:48:10 +0530

    Fixing time slot errors

--- src/routes/schedule/new.ts
+++ src/routes/schedule/new.ts
@@ -3,7 +3,6 @@ import {body} from "express-validator";
 import {RequestValidationError, validateRequest} from "../../common";
 import {Schedule} from "../../models/schedule";
 import {AlreadyBlocked} from "../../common/errors/already-blocked";
-import {User} from '../../models/user';
 
 const router = express.Router();
 
@@ -18,9 +17,9 @@ router.post(
     validateRequest,
     async (req: Request, res: Response) => {
         const {executive_id, retailer_name, in_time, out_time} = req.body;
-        const inTime: Date = new Date(in_time);
-        const outTime: Date = new Date(out_time);
-        if (inTime.valueOf() - outTime.valueOf() == 1) {
+        const newInTime: Date = new Date(in_time);
+        const newOutTime: Date = new Date(out_time);
+        if (newInTime.valueOf() - newOutTime.valueOf() == 1) {
             throw new RequestValidationError([{
                 msg: "Slot too tiny",
                 location: "body",
@@ -29,9 +28,9 @@ router.post(
             }])
         }
         if (
-            inTime.getDay() != outTime.getDay() ||
-            inTime.getMonth() != outTime.getMonth() ||
-            inTime.getFullYear() != outTime.getFullYear()
+            newInTime.getDay() != newOutTime.getDay() ||
+            newInTime.getMonth() != newOutTime.getMonth() ||
+            newInTime.getFullYear() != newOutTime.getFullYear()
         ) {
             throw new RequestValidationError([{
                 msg: "Start time and end time should be same date",
@@ -40,11 +39,55 @@ router.post(
                 value: ""
             }])
         }
-        if (await Schedule.count({
-            "retailer_name": retailer_name,
-            "in_time": {$lte: in_time},
-            "out_time": {$gte: out_time}
-        }) > 0) {
+        const count = await Schedule.count({
+            $and:
+                [
+                    {
+                        "retailer_name":
+                            {
+                                $eq: retailer_name
+                            }
+                    },
+                    {
+                        $or:
+                            [
+                                {
+                                    $and: [
+                                        {
+                                            "in_time":
+                                                {
+                                                    $gte: newInTime
+                                                }
+                                        },
+                                        {
+                                            "in_time":
+                                                {
+                                                    $lte: newOutTime
+                                                }
+                                        }
+                                    ]
+                                },
+                                {
+                                    $and: [
+                                        {
+                                            "out_time":
+                                                {
+                                                    $gte: newInTime
+                                                }
+                                        },
+                                        {
+                                            "out_time":
+                                                {
+                                                    $lte: newOutTime
+                                                }
+                                        }
+                                    ]
+                                }
+                            ]
+                    }
+                ]
+        });
+        if (count > 0) {
             throw new AlreadyBlocked("retailer visit is already planned");
         }
 
